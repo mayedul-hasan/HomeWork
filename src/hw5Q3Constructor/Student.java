02f079ee0c00eac13fc4a32e66a470accc59de48
MayedulHasan <maydulh5@gmail.com>
2022-05-31 21:30:14 +0200

    2nd

--- src/hw5Q3Constructor/Student.java
+++ src/hw5Q3Constructor/Student.java
@@ -1,5 +1,30 @@
 package hw5Q3Constructor;
 
 public class Student {
+	public String stName;
+	public int stID;
+	public  char sex;
+	public boolean isProgrammer;
+	public float grade;
+	
+	//default constructor
+	public Student() {
+		System.out.println("This is student details--");	
+	}
+	
+	//Parameterized constructor	
+	public Student(String stName, int stID, char sex, boolean
+			isProgrammer, float grade) {
+		this.stName = stName;
+		this.stID = stID;
+		this.sex =sex;
+		this.isProgrammer =isProgrammer;
+		this.grade=grade;
+		
+		System.out.println("Student Name is:" + stName + "\nStudent ID:" + stID +
+				"\nSex:" + sex + "\nIs he Programmer:" + isProgrammer + "\nStudent Grade:" + grade);
+	}
+	
+	
 
 }
--- src/hw5Q3Constructor/StudentTest.java
+++ src/hw5Q3Constructor/StudentTest.java
@@ -1,5 +1,12 @@
 package hw5Q3Constructor;
 
 public class StudentTest {
+	
+	public static void main(String[] args) {		
+		Student student =new Student();
+		Student student1 =new Student("Mayedul", 12 , 'M', true, 2.63f);
+	}
+	
+	
 
 }
