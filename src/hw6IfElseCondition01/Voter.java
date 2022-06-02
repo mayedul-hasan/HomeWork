fa129ecb553088caf128dd9cfc8dea37d2b21800
MayedulHasan <maydulh5@gmail.com>
2022-06-02 23:23:19 +0200

    nm

--- src/hw6IfElseCondition01/Voter.java
+++ src/hw6IfElseCondition01/Voter.java
@@ -1,16 +1,22 @@
 package hw6IfElseCondition01;
 
+import java.util.Scanner;
+
 public class Voter {
 	public static void main(String[] args) {
 		
-		System.out.println("Please add a valid age--");
-		int age = 21;
+		System.out.println("\bPlease add a valid age\b");
+		System.out.println("................................");
+		Scanner scanner =new Scanner(System.in);
+		int age = scanner.nextInt();
 		
-		if (age > 18) {
+		if (age >= 18) {
 			System.out.println("\nI am a Voter");
 		}	
 		else {
 			System.out.println("\nI am not a Voter");
+			
+		scanner.close();
 		}
 		
 	}
